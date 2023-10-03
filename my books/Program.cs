6ac609809ae3298e9a2f3624901540d260582d74
Aokiri <usagiyneko@gmail.com>
2023-10-03 16:52:05 +0200

    A little update to appsettings.json to work with versions. 🔢

--- my books/Program.cs
+++ my books/Program.cs
@@ -5,7 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "my_books_v1", Version = "v1"});
+});
 
 var app = builder.Build();
 
@@ -13,7 +16,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "my_books_v1"));
 }
 
 app.UseHttpsRedirection();
